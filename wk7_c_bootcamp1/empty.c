3bb165eef97524ed4fe65048392cf4c9a4cf63a5
sharontuwei <146426268+sharontuwei@users.noreply.github.com>
2023-11-14 16:12:22 +0000

    wk7_workingfiles

---
+++ wk7_c_bootcamp1/empty.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+typedef struct{
+    char date[20];
+    float bloodIron;
+} reading;
+
+
+int main(){
+    reading one_reading;
+    reading weekly_reading [7];
+
+return 0;
+}
